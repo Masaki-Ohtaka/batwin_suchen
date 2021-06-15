5248254c15e93521b4a7f74f5a74f4084aaf9cc7
Masaki-Ohtaka <tixeko1@yahoo.co.jp>
2021-06-15 21:59:43 +0900

    施設情報編集機能実装

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   # def new
@@ -38,21 +38,25 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+   protected
 
-  def update_resource(resource, params)
+    
+
+  # If you have extra params to permit, append them to the sanitizer.
+   def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :profile, :birthday, :gender, :first_name, :last_name, :first_kana, :last_kana, :postal_code, :location_id, :municipality, :address, :building_name, :phone_number])
+   end
+
+   
+
+  # If you have extra params to permit, append them to the sanitizer.
+   def configure_account_update_params
+     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :profile, :birthday, :gender, :first_name, :last_name, :first_kana, :last_kana, :postal_code, :location_id, :municipality, :address, :building_name, :phone_number])
+   end
+
+   def update_resource(resource, params)
     resource.update_without_password(params)
-  end
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+   end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
