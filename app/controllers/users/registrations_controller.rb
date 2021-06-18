1df802bd5389cbea0afdefbe06cd7934af95733f
Masaki-Ohtaka <tixeko1@yahoo.co.jp>
2021-06-18 17:20:24 +0900

    ユーザーのみ、施設者のみがそれぞれログインしてる状態でページ遷移するものを指定（新規登録、編集）

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :authenticate_foundation!
   before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
 
@@ -44,7 +45,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
    def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :profile, :birthday, :gender_id, :first_name, :last_name, :first_kana, :last_kana, :postal_code, :location_id, :municipality, :address, :building_name, :phone_number])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :profile, :birthday, :gender_id, :first_name, :last_name, :first_kana, :last_kana, :postal_code, :location_id, :municipality, :address, :building_name, :phone_number])
    end
 
    
@@ -57,6 +58,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
    def update_resource(resource, params)
     resource.update_without_password(params)
    end
+
+
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
-  get 'job_change_dogs/index'
-  devise_for :foundations, controllers: {
+  devise_for :foundations, controllers:{
     sessions:      'foundations/sessions',
     passwords:     'foundations/passwords',
     registrations: 'foundations/registrations'
-  }
-
+  } 
   devise_for :users, controllers: {
     sessions:      'users/sessions',
     passwords:     'users/passwords', 
     registrations: 'users/registrations'
   }
   root to: "job_change_dogs#index"
-  
+  # resources :foundations do
+  #   resources :job_change_dogs
+  # end
+
 end
