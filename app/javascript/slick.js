0b012ae9bd6b7bd7e2220a29458caba17c40f8cf
Masaki-Ohtaka <tixeko1@yahoo.co.jp>
2021-07-16 10:37:27 +0900

    全体的レイアウト修正

--- app/javascript/slick.js
+++ app/javascript/slick.js
@@ -1,3 +1,6 @@
-$(function() {
+// window.addEventListener('DOMContentLoaded', function(){ //$(function(){から変更エラーメッセージが変わった
+//   // $('.slider').slick();
+// });
+$(function (){
   $('.slider').slick();
-});
+})
